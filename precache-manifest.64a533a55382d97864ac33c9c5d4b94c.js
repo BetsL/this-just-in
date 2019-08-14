a2af3483f3b7e3c0e2067778233cc090a8849012
Bets <elizabeth.lidstrom@gmail.com>
2019-08-14 15:59:20 -0400

    Updates

---
+++ precache-manifest.64a533a55382d97864ac33c9c5d4b94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0111038fd05357ba55c9ef789bf74bb",
+    "url": "/this-just-in/index.html"
+  },
+  {
+    "revision": "f6beb7c55f437e9815af",
+    "url": "/this-just-in/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "bd755eb2f03335563571",
+    "url": "/this-just-in/static/css/main.2611b62b.chunk.css"
+  },
+  {
+    "revision": "f6beb7c55f437e9815af",
+    "url": "/this-just-in/static/js/2.7e4377fe.chunk.js"
+  },
+  {
+    "revision": "bd755eb2f03335563571",
+    "url": "/this-just-in/static/js/main.a00e360e.chunk.js"
+  },
+  {
+    "revision": "7e62341df4b46c60c70b",
+    "url": "/this-just-in/static/js/runtime~main.31206750.js"
+  }
+]);
